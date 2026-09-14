8e2c25b5002b0053e2cbe65c62e3fdaacebd1297
Monique <125992576+monique-ferreira@users.noreply.github.com>
2026-09-14 08:28:00 -0300

    att separação de estilos: barrel index telas medico

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -5,8 +5,12 @@
  * adicione o export aqui para manter tudo organizado.
  */
 export { styles as consultaCardStyles } from "./consultaCard.styles";
+export { styles as loginMedicoStyles } from "./loginMedico.styles";
+export { styles as cadastroMedicoStyles } from "./cadastroMedico.styles";
+export { styles as perfilMedicoStyles } from "./perfilMedico.styles";
+export { styles as consultasMedicoStyles } from "./consultasMedico.styles";
 
 // Adicione abaixo conforme for criando os demais arquivos de estilo:
 // export { styles as homeScreenStyles } from "./homeScreen.styles";
-// export { styles as loginMedicoStyles } from "./loginMedico.styles";
+// export { styles as loginPacienteStyles } from "./loginPaciente.styles";
 // ...
